8f8c7e27796772d5c2d75c12dd7d0eb61fd18076
Kooale325 <muhammad.bilal832017@gmail.com>
2024-03-18 16:00:13 +0500

    fixed tokenauth

--- api/middlewares/tokenauth.js
+++ api/middlewares/tokenauth.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const tokenAuth = async (req, res, next) => {
   try {
     const token = req.header("x-auth-token");
-    if (!token) return res.status(401).json({ msg: "No authentication token, access denied" });
+    if (!token)
+      return res
+        .status(401)
+        .json({ msg: "No authentication token, access denied" });
 
-    const verified = jwt.verify(token, "thisisthesecretkey");
-    if (!verified) return res.status(401).json({ msg: "Token verification failed, authorization denied" });
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified)
+      return res
+        .status(401)
+        .json({ msg: "Token verification failed, authorization denied" });
 
     req.user = verified.user;
     req.token = token;
