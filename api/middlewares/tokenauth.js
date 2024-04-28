58360b67218722991ac581ddffbbaab5a17b6bd7
Kooale325 <muhammad.bilal832017@gmail.com>
2024-04-28 14:14:42 +0500

    Update tokenauth.js

--- api/middlewares/tokenauth.js
+++ api/middlewares/tokenauth.js
@@ -5,6 +5,7 @@ const tokenAuth = async (req, res, next) => {
     const token = req.header("x-auth-token");
     if (token === null) return res.status(401).json({ msg: "No authentication token, access denied" });
     const verified = jwt.verify(token, process.env.jwtsecret);
+    if (!verified) return res.status(401).json({ msg: "Token verification failed, authorization denied" });
     req.user = verified.user;
     req.token = token;
     next();
