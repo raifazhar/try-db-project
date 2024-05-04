169d14bd6746ae18b8b80f3577047a5fed2487b2
raifazhar <raifazhar.612@gmail.com>
2024-05-04 10:35:55 +0500

    browser 2 procedure done

--- api/routes/browser.js
+++ api/routes/browser.js
@@ -3,19 +3,35 @@ const browserRouter = express.Router();
 const connection = require("../db");
 
 browserRouter.get("/api/browser", (req, res) => {
-    var query=`Select * from \`browserdata\``;
+    let sqlQuery='call getbrowserdata'
     new Promise((resolve, reject) => {
-        connection.query(query, (err, result, fields) => {
+        connection.query(sqlQuery, (err, result, fields) => {
           if (err) reject(err);
           else resolve(result);
         });
       }).then((result) => {
-        res.send(result);
+        res.send(result[0]);
       }).catch((e)=>{
         res.status(404).send(e);
         return;
       });
-
 })
 
+browserRouter.get("/api/search", (req, res) => {  
+  let sqlQuery='call search(?)'
+
+  const searchstring=req.query.searchstring
+  console.log(searchstring)
+  new Promise((resolve, reject) => {
+      connection.query(sqlQuery,[searchstring] ,(err, result, fields) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    }).then((result) => {
+      res.send(result[0]);
+    }).catch((e)=>{
+      res.status(404).send(e);
+      return;
+    });
+})
 module.exports = browserRouter;
