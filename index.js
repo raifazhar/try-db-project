1bd61d7005e07161b6c96e04bc8ef7667ad7e744
Kooale325 <muhammad.bilal832017@gmail.com>
2024-05-05 14:23:45 +0500

    istg if it does not work

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const express = require("express");
-const authRouter = require("./routes/auth");
-const browserRouter = require("./routes/browser");
-const placeRouter = require("./routes/place");
-const tokenAuth = require("./middlewares/tokenauth");
-const profileRouter = require("./routes/profile");
+// const authRouter = require("./routes/auth");
+// const browserRouter = require("./routes/browser");
+// const placeRouter = require("./routes/place");
+// const tokenAuth = require("./middlewares/tokenauth");
+// const profileRouter = require("./routes/profile");
 const app = express();
 const port = process.env.PORT || 3000;
 var cors = require("cors");
@@ -16,24 +16,24 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send(port);
 });
-app.use(authRouter);
-app.use(browserRouter);
-app.use(placeRouter);
-//middlewares
-app.use(tokenAuth);
+// app.use(authRouter);
+// app.use(browserRouter);
+// app.use(placeRouter);
+// //middlewares
+// app.use(tokenAuth);
 
 app.get("/api/verify", (req, res) => {
   res.send("Verified!");
 });
 
 //Routers
-app.use(profileRouter);
+// app.use(profileRouter);
 
 const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
-const io = require("socket.io")(server, {
-  path: "/api/socket.io",
-  cors: {
-    origin: "*",
-  },
-});
-require("./socket/socket")(io);
+// const io = require("socket.io")(server, {
+//   path: "/api/socket.io",
+//   cors: {
+//     origin: "*",
+//   },
+// });
+// require("./socket/socket")(io);
