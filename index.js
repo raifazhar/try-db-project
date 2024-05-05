bec2176262b804f189822eaf34ec0000f862e3c2
Kooale325 <muhammad.bilal832017@gmail.com>
2024-05-05 14:39:13 +0500

    unghhhhhhhhhh

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //require("dotenv").config();
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const express = require("express");
 // const authRouter = require("./routes/auth");
 // const browserRouter = require("./routes/browser");
@@ -8,9 +8,9 @@ const express = require("express");
 // const profileRouter = require("./routes/profile");
 const app = express();
 const port = process.env.PORT || 3000;
-var cors = require("cors");
-app.use(bodyParser.json());
-app.use(cors());
+// var cors = require("cors");
+// app.use(bodyParser.json());
+// app.use(cors());
 
 //http://localhost:3000/api/
 app.get("/", (req, res) => {
