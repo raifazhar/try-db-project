f6883f5b8a24e1adb0c6d95b66c5a17c289f203a
Kooale325 <muhammad.bilal832017@gmail.com>
2024-05-07 07:37:26 +0500

    added semicolons and formatted code

--- api/routes/browser.js
+++ api/routes/browser.js
@@ -3,35 +3,39 @@ const browserRouter = express.Router();
 const connection = require("../db");
 
 browserRouter.get("/api/browser", (req, res) => {
-    let sqlQuery='call getbrowserdata'
-    new Promise((resolve, reject) => {
-        connection.query(sqlQuery, (err, result, fields) => {
-          if (err) reject(err);
-          else resolve(result);
-        });
-      }).then((result) => {
-        res.send(result[0]);
-      }).catch((e)=>{
-        res.status(404).send(e);
-        return;
-      });
-})
-
-browserRouter.get("/api/search", (req, res) => {  
-  let sqlQuery='call search(?)'
-
-  const searchstring=req.query.searchstring
-  console.log(searchstring)
+  let sqlQuery = "call getbrowserdata";
   new Promise((resolve, reject) => {
-      connection.query(sqlQuery,[searchstring] ,(err, result, fields) => {
-        if (err) reject(err);
-        else resolve(result);
-      });
-    }).then((result) => {
+    connection.query(sqlQuery, (err, result, fields) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  })
+    .then((result) => {
       res.send(result[0]);
-    }).catch((e)=>{
+    })
+    .catch((e) => {
       res.status(404).send(e);
       return;
     });
-})
+});
+
+browserRouter.get("/api/search", (req, res) => {
+  let sqlQuery = "call search(?)";
+
+  const searchstring = req.query.searchstring;
+  console.log(searchstring);
+  new Promise((resolve, reject) => {
+    connection.query(sqlQuery, [searchstring], (err, result, fields) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  })
+    .then((result) => {
+      res.send(result[0]);
+    })
+    .catch((e) => {
+      res.status(404).send(e);
+      return;
+    });
+});
 module.exports = browserRouter;
