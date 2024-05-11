c70a70895e3bdc1f2e5027ad27f17f4248e95f1f
raifazhar <raifazhar.612@gmail.com>
2024-05-11 21:19:16 +0500

    place backend-done

--- api/routes/place.js
+++ api/routes/place.js
@@ -3,22 +3,11 @@ const PlaceRouter = express.Router();
 const connection = require("../db");
 
 PlaceRouter.get("/api/placepage", async (req, res) => {
-  let sqlQuery = "call Getpagedata(?)";
-  const pageid = req.query.pageid;
+  let sqlQuery = "select * from Destinations where PlaceID = ?";
+  const placeid = req.query.pageid;
   try {
-    let results = await connection.query(sqlQuery, [pageid]);
-    let result = results[0];
-    const newresponse = {
-      pageid: result[0][0].pageID,
-      description: result[0][0].description,
-    };
-
-    // Iterate through the remaining elements
-    for (let i = 0; i < result[0].length; i++) {
-      const element = result[0][i];
-      newresponse[element.pictureID] = element.url;
-    }
-    res.send(newresponse);
+    let results = await connection.query(sqlQuery, [placeid]);
+    res.send(results[0]);
   } catch (e) {
     res.status(404).send(e);
     return;
